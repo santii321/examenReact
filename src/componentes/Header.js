6c4a49e814e487969e86fd4544fa24b62d6eff1b
santii321 <iefaz.santiagogaviria@gmail.com>
2022-11-25 01:12:46 -0500

    se cambia div por section para una mejor semantica

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -4,14 +4,14 @@ function Header() {
   return (
     <header>
       <section className="headerLogo">
-        <div className="slogan">
+        <section className="slogan">
           <h1 className="fw-light text-white">HappyAgro</h1>
           <p className="lead text-white">Somos una iniciativa que busca cultivar y distribuir en todo Colombia nuestra maravillosa planta emocional.</p>
           <p>
             <Link className="btn btn-primary my-2" to="/productos">Conoce nuestros productos...</Link>
 
           </p>
-        </div></section>
+        </section></section>
     </header>
   );
 }
